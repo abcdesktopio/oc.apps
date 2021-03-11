9af75da342a75ea6bd9fbaf3c286968f2529bee8
alexandre devely <alexandre.devely@orange.com>
2021-03-11 09:07:38 +0100

    local2prod.sh

---
+++ local2prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -O images-list.txt https://raw.githubusercontent.com/abcdesktopio/conf/main/images-list.txt
+while read i; do
+        docker tag abcdesktopio/$i:dev abcdesktopio/$i
+        docker push abcdesktopio/$i
+done < ../images-list.txt
