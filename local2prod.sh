09f383d97fcd9499d13cb35301b959a75c15bd66
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2021-06-03 16:56:30 +0200

    change limits

--- local2prod.sh
+++ local2prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O images-list.txt https://raw.githubusercontent.com/abcdesktopio/conf/main/images-list.txt
+# wget -O images-list.txt https://raw.githubusercontent.com/abcdesktopio/conf/main/images-list.txt
 while read i; do
         docker tag abcdesktopio/$i:dev abcdesktopio/$i
         docker push abcdesktopio/$i
