1a11e9a941de9f669c299bfcc8c7e12a555874df
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-01-07 11:41:20 +0100

    fix format 3.0

--- make-docs.js
+++ make-docs.js
@@ -15,8 +15,10 @@
 /* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
-const dockerRegistryPath = 'abcdesktopio';
-const hostedurl = "https://raw.githubusercontent.com/abcdesktopio/oc.apps/main";
+const childProcess = require('child_process');
+const DOCKERREGISTRYPATH = 'abcdesktopio';
+const HOSTEDURL = "https://raw.githubusercontent.com/abcdesktopio/oc.apps/main";
+const RELEASE='3.0';
 
 // function to encode file data to base64 encoded string
 function base64Encode(file) {
@@ -38,10 +40,42 @@ function makedummy(e) {
 }
 
 
+function writecmd( fd, cmd, type ) {
+  fs.writeSync( fd, "\n");
+  if (!type) type = ''; 
+  fs.writeSync( fd, "``` " + type + "\n");
+  fs.writeSync( fd, cmd + "\n" );
+  fs.writeSync( fd, "```\n\n");
+}
+
+
+function getrelease( appname ) {
+  var release = undefined;
+  // docker run -it --rm abcdesktopio/firefox.d:3.0 bash -c "cat /etc/lsb-release 2>/dev/null || cat /etc/os-release 2>/dev/null"
+  appname = appname.toLowerCase();
+  var command = 'docker run --rm ' + DOCKERREGISTRYPATH + '/' + appname + '.d:' + RELEASE;
+  command += ' /bin/cat /etc/os-release';
+  try {
+    console.log(command);
+    stdout = childProcess.execSync(command).toString();
+    release = stdout;	  
+  } catch (error) {
+    console.error( `error in getrelease ${DOCKERREGISTRYPATH}/${appname}:3.0`);
+    console.error( error );
+    // error.status;  // 0 : successful exit, but here in exception it has to be greater than 0
+    // error.message; // Holds the message you typically want.
+    // error.stderr;  // Holds the stderr output. Use `.toString()`.
+    // error.stdout;  // Holds the stdout output. Use `.toString()`.
+ }
+ console.log (release);
+ return release;
+}
+
 function makedocumentation(e) {
   console.log(e);
   const filename = e.name.toLowerCase() + '.md';
   const dockerfilename = e.name.toLowerCase() + '.d';
+  const jsonfilename = e.name.toLowerCase() + '.d.' + RELEASE + '.json';
 
   console.log( 'createfile ' + filename );
   fd = fs.openSync(filename,'w');
@@ -49,40 +83,48 @@ function makedocumentation(e) {
   fs.writeSync( fd, `![${e.icon}](icons/${e.icon}){: style="height:64px;width:64px"}\n` );
 
   if (e.template) {
-    fs.writeSync( fd, `## inherite from\n[${e.template}](${'../' + e.template}.md)\n`);
+    fs.writeSync( fd, `## inherite from\n[${e.template}](${'../' + e.template})\n`);
   }
   if (e.debpackage) {
     fs.writeSync( fd,'## Distribution\nubuntu ![ubuntu](icons/ubuntu.svg){: style="height:32px;"}\n');
+    
+    release = getrelease(e.name);
+    if (release) {
+      writecmd( fd, release );
+      fs.writeSync( fd, '\n');
+    }
+
     fs.writeSync( fd,'## Ubuntu packages\n');
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, e.debpackage );
-    fs.writeSync( fd, "\n```\n");
+    writecmd( fd, e.debpackage );
   }
   if (e.apkpackage) {
     fs.writeSync( fd,'## Distribution\nalpine ![alpine](icons/alpine.svg){: style="height:32px;"}\n');
+    release = getrelease(e.name);
+    if (release) {
+      writecmd( fd, release );
+      fs.writeSync( fd, '\n');
+    }
     fs.writeSync( fd,'## Alpine packages\n');
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, e.apkpackage );
-    fs.writeSync( fd, "\n```\n");
+    writecmd( fd, e.apkpackage );
+  }
 
-  }
+  if (e.licence === "non-free"){
+    fs.writeSync( fd,'## Licence\n ** This application is NO FREE. ** You need to build it manually.\n\n');
+  } 
+
   if (e.description) {
-    fs.writeSync( fd, `## Description\n${e.description}\n`);
+    fs.writeSync( fd, `## Comment\n${e.description}\n`);
   }
-  if (e.args) fs.writeSync( fd, `## Arguments\n${JSON.stringify(e.args)}\n`);
+  if (e.args) fs.writeSync( fd, "## Arguments\n`" + JSON.stringify(e.args) + "`\n");
   
   if (e.displayname) {
     fs.writeSync( fd, "## Displayname\n\n" );
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, JSON.stringify(e.displayname) ); 
-    fs.writeSync( fd, "\n```\n");
+    writecmd( fd, e.displayname);
   }
   
   if (e.path) {
     fs.writeSync( fd, "## Path\n\n");
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, JSON.stringify(e.path) ); 
-    fs.writeSync( fd, "\n```\n");
+    writecmd( fd, e.path);
   }
 
   if (e.uniquerunkey) { fs.writeSync( fd, `## uniquerunkey\n${JSON.stringify(e.uniquerunkey)}\n`); }
@@ -90,105 +132,81 @@ function makedocumentation(e) {
   if (e.execmode) { fs.writeSync( fd, `## Exec mode\n${JSON.stringify(e.execmode)}\n`); }
   if (e.mimetype) { 
     fs.writeSync( fd, '## Mimetype\n');
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, e.mimetype); 
-    fs.writeSync( fd, "\n```\n");
+    writecmd( fd, e.mimetype);
   }
   if (e.fileextensions) {
-    fs.writeSync( fd, `## File extensions\n${JSON.stringify(e.fileextensions)}\n`);
+    fs.writeSync( fd, "## File extensions\n`" +  JSON.stringify(e.fileextensions) + "`\n" );
   }
   if (e.legacyfileextensions) {
-    fs.writeSync( fd, 
-      `## Legacy file extensions\n${JSON.stringify(e.legacyfileextensions)}\n`
-    );
+    fs.writeSync( fd, "## Legacy file extensions\n`" +  JSON.stringify(e.legacyfileextensions) + "`\n" );
   }
   if (e.acl) { 
-    fs.writeSync( fd, '## ACL\n');
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, JSON.stringify(e.acl, null, 4));
-    fs.writeSync( fd, "\n```\n");
+    fs.writeSync( fd, "## ACL\n");
+    writecmd( fd,  JSON.stringify(e.acl, null, 4), 'json' )
   }
   if (e.launch) { 
-    fs.writeSync( fd, '## WM_CLASS\n' );
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, e.launch );
-    fs.writeSync( fd, "\n```\n");
-    fs.writeSync( fd, '> The WM_CLASS property (of type STRING without control characters) contains two consecutive null-terminated strings. These specify the Instance and Class names to be used by both the client and the window manager for looking up resources for the application or as identifying information.\n'); 
-    fs.writeSync( fd, '> to get the WM_CLASS property of an application, use the command line `wmctrl -lx`');
-    fs.writeSync( fd, "\n\n");
+    fs.writeSync( fd, "## WM_CLASS\n" );
+    writecmd( fd,  e.launch )
+    fs.writeSync( fd, "> The WM_CLASS property (of type STRING without control characters) contains two consecutive null-terminated strings. These specify the Instance and Class names to be used by both the client and the window manager for looking up resources for the application or as identifying information.\n"); 
+    fs.writeSync( fd, "> to get the WM_CLASS property of an application, use the command line `wmctrl -lx`\n\n");
   }
   if (e.desktopfile) { 
-    fs.writeSync( fd, '## Desktopfile\n');
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd,  e.desktopfile );
-    fs.writeSync( fd, '\n');
-    fs.writeSync( fd, "```\n");
-    fs.writeSync( fd, '> A .desktop file is a simple text file that holds information about a program. It is usually placed in “/usr/share/applications/”.');
-    fs.writeSync( fd, '\n');
+    fs.writeSync( fd, "## Desktopfile\n");
+    writecmd( fd, e.desktopfile );
+    fs.writeSync( fd, '> A .desktop file is a simple text file that holds information about a program. It is usually placed in “/usr/share/applications/”.\n\n');
   }
  
   if (e.preruncommands) {
-    fs.writeSync( fd, `## PRE run command\n`);
-    fs.writeSync( fd, '\n');
-    fs.writeSync( fd, '> PRE run command are run **before** the package install command\n');
-    fs.writeSync( fd, '\n');
+    fs.writeSync( fd, "## PRE run command\n\n");
+    fs.writeSync( fd, '> PRE run command are run **before** the package install command\n\n');
     fs.writeSync( fd, "```\n");
     e.preruncommands.forEach(element => fs.writeSync( fd, element + '\n') );
     fs.writeSync( fd, "```\n\n");
   }
   if (e.postruncommands) {
-    fs.writeSync( fd, `## POST run command\n`);
-    fs.writeSync( fd, '\n');
-    fs.writeSync( fd, '> POST run command are run **after** the package install comman\n');
-    fs.writeSync( fd, "```");
-    fs.writeSync( fd, '\n\n');
+    fs.writeSync( fd, "## POST run command\n\n");
+    fs.writeSync( fd, '> POST run command are run **after** the package install comman\n\n');
+    fs.writeSync( fd, "```\n");
     e.postruncommands.forEach(element => fs.writeSync( fd, element + '\n'));
-    fs.writeSync( fd, "```");
-    fs.writeSync( fd, '\n\n');
+    fs.writeSync( fd, "```\n\n");
   }
 
-  fs.writeSync( fd, "\n");
+  fs.writeSync( fd, "\n\n");
   fs.writeSync( fd, "## JSON dump\n");
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, "json source file\n\n");
-  fs.writeSync( fd, "``` json");
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, JSON.stringify(e, null, 4) );
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, "```\n\n");
+  fs.writeSync( fd, "json source file " + jsonfilename + " \n");
+  writecmd( fd, JSON.stringify(e, null, 4), "json" );
 
+
+  fs.writeSync( fd, '## Install the builded image\n');
+  fs.writeSync( fd, '>Replace the **ABCHOST** var set to localhost by default to your own server ip address\n');
+  installcmd =  `ABCHOST=localhost\n`;
+  installcmd += `curl --output ${jsonfilename} ${HOSTEDURL}/${jsonfilename}\n`;
+  installcmd += "curl -X PUT -H 'Content-Type: text/javascript' http://$ABCHOST:30443/API/manager/image -d @" + jsonfilename + "\n";
+  writecmd( fd, installcmd, 'sh' );
+
+  fs.writeSync( fd, '## Generated `DockerFile` source code\n');
   const dockefiledatadata = fs.readFileSync(dockerfilename,{encoding:'utf8', flag:'r'});
-  fs.writeSync( fd, '## Rebuild the image\n\n');
+  writecmd( fd, dockefiledatadata );
 
-  fs.writeSync( fd, '### Download the Dockerfile');
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, `[Dockerfile for application ${e.name}](${hostedurl}/${e.name}.d)`);
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, "``` sh\n");
-  fs.writeSync( fd, `wget ${hostedurl}/${e.name}.d` );
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, "```\n");
+  fs.writeSync( fd, '## Rebuild the image manually\n\n');
+  fs.writeSync( fd, '### Download the Dockerfile manually\n');
+  fs.writeSync( fd, `[Dockerfile for application ${e.name}](${HOSTEDURL}/${e.name}.d)`);
+  writecmd( fd, `wget ${HOSTEDURL}/${e.name}.d`, 'sh' );
 
-
-  fs.writeSync( fd, '### build this `Dockerfile` to create a container image\n');
-  fs.writeSync( fd, "``` sh\n");
-  fs.writeSync( fd, `docker build --build-arg TAG=3.0 -f ${e.name}.d -t ${e.name} .` );
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, "```\n");
+  fs.writeSync( fd, '### build the `Dockerfile` to create a container image\n');
+  writecmd( fd, `docker build --build-arg TAG=${RELEASE} -f ${e.name}.d -t ${e.name} .`, 'sh' );
 
   fs.writeSync( fd, '### Install the new image\n');
-  fs.writeSync( fd, '>Replace localhost by your own server ip address\n');
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, "``` sh\n");
-  fs.writeSync( fd, `docker inspect ${e.name} > ${e.name}.json\n` );
-  fs.writeSync( fd, `curl -X PUT -H 'Content-Type: text/javascript' http://localhost:30443/API/manager/image -d @${e.name}.json\n` );
-  fs.writeSync( fd, "```\n");
-
-  fs.writeSync( fd, "\n");
-  fs.writeSync( fd, '## Generated DockerFile source code\n');
-  fs.writeSync( fd, "```\n");
-  fs.writeSync( fd, dockefiledatadata );
-  fs.writeSync( fd, "```\n");
+  fs.writeSync( fd, '>If you are using `containerd` as container runtime, use the ctr command line\n');
+  fs.writeSync( fd, '\n \n');
+  fs.writeSync( fd, '>If you are not running this bash command on your abcdesktop node\n');
+  fs.writeSync( fd, '>Replace the **ABCHOST** variable set to localhost by default to your own server ip address\n\n');
+  installcmd =  `ABCHOST=localhost\n`;
+  installcmd += `docker inspect ${e.name} > ${e.name}.json\n`;
+  installcmd += `docker image save ${e.name} -o ${e.name}.tar\n`;
+  installcmd += `ctr -n k8s.io images import ${e.name}.tar\n`;
+  installcmd += "curl -X PUT -H 'Content-Type: text/javascript' http://$ABCHOST:30443/API/manager/image -d @" + e.name + ".json\n";
+  writecmd( fd, installcmd, 'sh' );
 
   fs.closeSync( fd );
 }
@@ -202,31 +220,32 @@ function makedocArray(e) {
   const icon = e.icon;
   const mimetype = e.mimetype;
   const appname = e.name.toLowerCase() + '.d';
-  const jsonfile = `[${filename}.d.3.0.json](${'../' + filename}.d.3.0.json)`;
+  const jsonfile = `[${filename}.d.${RELEASE}.json](${'../' + filename}.d.${RELEASE}.json)`;
   const mdfile = `[${filename}.md](${'../' + filename})`;
   const iconfile = `![${e.icon}](${'icons/' + e.icon}){: style="height:32px;width:32px"}`;
-  var description = "no description found";
+  var description = "no comment";
   if (e.desktopfile) {
-    console.log( `${dockerRegistryPath}/${appname}:3.0 ${e.desktopfile}` );
-    var command = `docker run --rm ${dockerRegistryPath}/${appname}:3.0 cat ${e.desktopfile} | grep 'Comment='`;
+    var command = `docker run --rm ${DOCKERREGISTRYPATH}/${appname}:3.0 cat ${e.desktopfile} | grep 'Comment='`;
     try {
-	var description = "no description found";
-    	stdout = require('child_process').execSync(command ).toString();
+	    var description = "no comment";
+      console.log(command);
+    	stdout = childProcess.execSync(command).toString();
     	const comment = 'Comment=';
     	const indexOfFirst = stdout.indexOf(comment);
     	if ( indexOfFirst != -1 ) {
     		var i = indexOfFirst + comment.length;
-		var end = stdout.length;
+		    var end = stdout.length;
     		description = stdout.substr(i, end-comment.length-1);
     	}
     } catch (error) {
-	console.error( `error in parsing 'Comment' in file ${e.desktopfile} image  ${dockerRegistryPath}/${appname}:3.0`);
-	// console.error( error );
+	    console.error( `error in parsing 'Comment' in file ${e.desktopfile} image ${DOCKERREGISTRYPATH}/${appname}:3.0`);
+	    // console.error( error );
     	// error.status;  // 0 : successful exit, but here in exception it has to be greater than 0
     	// error.message; // Holds the message you typically want.
     	// error.stderr;  // Holds the stderr output. Use `.toString()`.
     	// error.stdout;  // Holds the stdout output. Use `.toString()`.
    }
+
   }
 
   docarray[appname] = `|${iconfile}|${displayname}|${description}|${mdfile}|${jsonfile}|`;
@@ -266,8 +285,8 @@ getDescription(jsoncontent);
 const wstreamlist = fs.createWriteStream('list.md');
 wstreamlist.write('# Application list\n');
 wstreamlist.write('This array describe the application list ready to use with abcdesktop.\n\n');
-wstreamlist.write('|icon|displayname|description|markdown|json|\n');
-wstreamlist.write('|----|-----------|-----------|--------|----|\n');
+wstreamlist.write('|icon|displayname|comment|description|json file|\n');
+wstreamlist.write('|----|-----------|-------|-----------|---------|\n');
 
 var keys = Object.keys(docarray); // or loop over the object to get the array
 // keys will be in any order
