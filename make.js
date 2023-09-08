e9ad051283ee2f24594b89643ab64d6b9e8811a3
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-09-08 15:19:14 +0200

    APP is deprecated

--- make.js
+++ make.js
@@ -220,6 +220,13 @@ function makedockerfile(e) {
   // wstream.write( "RUN for d in /usr/share/icons /usr/share/pixmaps ; do echo \"testing link in $d\"; if [ -d $d ] && [ -x /composer/safelinks.sh ] ; then echo \"fixing link in $d\"; cd $d ; /composer/safelinks.sh ; fi; done\n" );
   if (e.name) wstream.write(`ENV APPNAME "${e.name}"\n`);
   if (e.path) wstream.write(`ENV APPBIN "${e.path}"\n`);
+  
+  // This is deprecated
+  //
+  if (e.path) {
+          wstream.write(`# ENV APP is deprecated, removed in next release\n`);
+          wstream.write(`ENV APP "${e.path}"\n`);
+  }
   if (e.args) { wstream.write(`LABEL oc.args=${JSON.stringify(e.args)}\n`); }
   if (e.usedefaultapplication) { wstream.write(`LABEL oc.usedefaultapplication=${JSON.stringify(e.usedefaultapplication)}\n`); }
   if (e.home) { wstream.write(`LABEL oc.home=${JSON.stringify(e.home)}\n`); }
